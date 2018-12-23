fa59b15552c77ebe92e87e346a44c977882a6cbd
Aaron Harris <37838136+alphaharris@users.noreply.github.com>
2018-12-23 10:05:34 -0600

    Added Part 2 (Command Line Interface)

---
+++ QR Codes Using Flask/qr_app_pt2/setup.py
@@ -0,0 +1,21 @@
+"""
+Note: Setup.py must be one level higher than qrwifi directory.
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    # mandatory
+    name="qrwifi",
+    # mandatory
+    version="0.1",
+    # mandatory
+    author_email="username@email.address",
+    packages=['qrwifi'],
+    package_data={},
+    install_requires=['pyqrcode', 'SolidPython', 'numpy', 'Flask', 'click'],
+    entry_points={
+        'console_scripts': ['qrwifi = qrwifi.cli:start']
+    }
+)
